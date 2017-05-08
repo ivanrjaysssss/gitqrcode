c49677fb8c4b499197d8f3f8cd998650d523633d
ivan rjay <ivan@evenlyten.com>
2017-05-08 09:35:07 -0700

    home and js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,9 @@ function init() {
     document.querySelector("#startScan").addEventListener("touchend", startScan, false);
     resultDiv = document.querySelector("#results");
 }
+
 function save_code(){
+
   //save
 
 
@@ -44,13 +46,44 @@ function startScan() {
             modalbutton.click();
         }*/
 
-        function (result) {
+        /*function (result) {
             var s = "Result: " + result.text + "<br/>" +
             "Format: " + result.format + "<br/>" +
             "Cancelled: " + result.cancelled;
             resultDiv.innerHTML = s;
             var modalbutton=document.getElementById("modal1");
             modalbutton.click();
+        },*/
+        function (result) {
+            var s =  result.text;
+            document.getElementById("results").value=" ";
+            document.getElementById("results").value=s;
+            document.getElementById("stud_code").value=" ";
+            document.getElementById("stud_code").value=s;
+            $.ajax({
+          url: 'http://qrcode.freesandboxdomain.com/login/userverify5',
+          type: 'POST',
+          data: {id: s},
+          success: function(response) {
+
+
+                                //localStorage.setItem(response);
+                                //console.log(response);
+                                var datas = JSON.parse(response);
+                                //var fname = datas.fname;
+                                //alert(datas.fname);
+                                
+                                  for(var i=0; i<datas.length; i++){
+                                    $('#namevalue').remove();
+                                    $('#name').append('<input type="text" id="namevalue" class= "form-control" value ="'+datas[i].lname+', '+datas[i].fname+' '+datas[i].mname+'" readonly>');
+                                    
+                                  }
+                                //document.write("Your Account id is: "+response);
+                                //alert(fname);
+
+          }
+    });
+            $('#myModal').modal('show')
         },
         function (error) {
             alert("Scanning failed: " + error);
